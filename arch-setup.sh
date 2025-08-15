a58779170af06807f0ce7fe9102eb90e15b42836
Billi-7 <145894722+Billi-7@users.noreply.github.com>
2025-08-15 22:31:37 +0530

    added setup script

---
+++ arch-setup.sh
@@ -0,0 +1,21 @@
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=us" >> /etc/vconsole.conf
+echo "earth" >> /etc/hostname
+echo "127.0.1.1       billi" >> /etc/hosts
+echo "set the root password"
+passwd
+useradd -m -g users -G wheel billi
+echo "set the user password"
+passwd billi
+echo "your_user_name ALL=(ALL) ALL" >> /etc/sudoers.d/billi
+sed -i 's/MODULES=()/MODULES=(btrfs)/g' /etc/mkinitcpio.conf
+mkinitcpio -p linux
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable NetworkManager
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
