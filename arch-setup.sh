26cded310b7458b87a84b9cbfc9f346f323254d6
Billi-7 <145894722+Billi-7@users.noreply.github.com>
2025-08-15 22:40:25 +0530

    Update arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -5,17 +5,17 @@ locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=us" >> /etc/vconsole.conf
 echo "earth" >> /etc/hostname
-echo "127.0.1.1       billi" >> /etc/hosts
+echo "127.0.1.1       earth" >> /etc/hosts
 echo "set the root password"
 passwd
 useradd -m -g users -G wheel billi
 echo "set the user password"
 passwd billi
-echo "your_user_name ALL=(ALL) ALL" >> /etc/sudoers.d/billi
+echo "billi ALL=(ALL) ALL" >> /etc/sudoers.d/billi
 sed -i 's/MODULES=()/MODULES=(btrfs)/g' /etc/mkinitcpio.conf
 mkinitcpio -p linux
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=x86_64-efi --boot-directory=/efi --efi-directory=your_boot_directory(/efi) --bootloader-id=GRUB
+grub-mkconfig -o /efi/grub/grub.cfg
 systemctl enable NetworkManager
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
