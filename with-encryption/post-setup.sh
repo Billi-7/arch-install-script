e6bfee96fb027082e357ea5b6011fe7d89dfd89a
Billi-7 <145894722+Billi-7@users.noreply.github.com>
2025-10-20 20:04:53 +0530

    Set up post-installation script for environment
    Install necessary packages and configure Snapper for Btrfs.

--- with-encryption/post-setup.sh
+++ with-encryption/post-setup.sh
@@ -1,1 +1,24 @@
+#!/usr/bin/env bash
 
+pacman -Sy
+
+sudo pacman -S hyprland kitty wofi snapper snap-pac pipewire pipewire-jack pipewire-pulse intel-ucode mlocate
+
+sudo pacman -Syu
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -Sy
+yay -S btrfs-assistant
+
+sudo snapper -c root create-config /
+sudo snapper -c home create-config /home
+
+sudo snapper -c root set-config ALLOW_USERS="$USER" SYNCACL=yes
+sudo snapper -c home set-config ALLOW_USERS="$USER" SYNCACL=yes
+
+#sudo nano /etc/updatedb.conf (now add .snapshots in prunenames)
+#sudo nano /etc/mkinitcpio.conf (now add grub-btrfs-overlayfs in hooks)
+sudo mkinitcoio -p linux
+sudo systemctl enable --now grub-btrfsd.service
