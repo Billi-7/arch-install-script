fd79e58d19fa7e3dafbc428de5d5b099675da4c6
Billi-7 <145894722+Billi-7@users.noreply.github.com>
2025-10-26 19:17:32 +0530

    Fix sudo command for mkinitcpio configuration

--- with-encryption/post-setup.sh
+++ with-encryption/post-setup.sh
@@ -20,7 +20,7 @@ sudo snapper -c home set-config ALLOW_USERS="$USER" SYNCACL=yes
 #sudo nano /etc/updatedb.conf (now add .snapshots in prunenames)
 sudo sed -i 's|PRUNENAMES = "|PRUNENAMES = ".snapshots |g' /etc/updatedb.conf
 #sudo nano /etc/mkinitcpio.conf (now add grub-btrfs-overlayfs in hooks)
-sed -i '55s/fsck/fsck grub-btrfs-overlayfs/' /etc/mkinitcpio.conf
+sudo sed -i '55s/fsck/fsck grub-btrfs-overlayfs/' /etc/mkinitcpio.conf
 sudo mkinitcpio -p linux
 sudo systemctl enable --now grub-btrfsd.service
 sudo grub-mkconfig -o /efi/grub/grub.cfg
